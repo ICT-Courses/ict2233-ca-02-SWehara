bcde70f9d0a514c4317306fb9bf935ec825fac23
SWehara <ar113035@fhss.sjp.ac.lk>
2025-09-23 08:26:42 +0530

    Add icons to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,39 @@
 import React from "react";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="p-4 text-center bg-gray-900 dark:bg-gray-800 text-white transition-colors duration-300">
-      © {new Date().getFullYear()} Senuri Wehara. All rights reserved •
-      <span className="ml-2">
+    <footer className="p-6 text-center bg-gray-900 dark:bg-gray-800 text-white transition-colors duration-300">
+      {/* Social Links */}
+      <div className="flex justify-center space-x-6 mb-3">
         <a
           href="https://github.com/SWehara"
           target="_blank"
           rel="noreferrer"
           className="text-blue-400 hover:text-blue-500 transition-colors duration-300"
         >
-          GitHub
+          <FaGithub size={20} />
         </a>
-      </span>
+        <a
+          href="https://www.linkedin.com/in/senuri-wehara-a339461a4"
+          target="_blank"
+          rel="noreferrer"
+          className="text-blue-400 hover:text-blue-500 transition-colors duration-300"
+        >
+          <FaLinkedin size={20} />
+        </a>
+        <a
+          href="mailto:senuriwehara03@gmail.com"
+          className="text-blue-400 hover:text-blue-500 transition-colors duration-300"
+        >
+          <FaEnvelope size={20} />
+        </a>
+      </div>
+
+      {/* Copyright */}
+      <div>
+        © {new Date().getFullYear()} Senuri Wehara. All rights reserved
+      </div>
     </footer>
   );
 }
